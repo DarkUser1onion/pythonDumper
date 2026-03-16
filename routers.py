38fede67be0c7956e9ccf981711c2911bf2dd3f9
DarkUser1onion <matvejselivanov4@yandex.ru>
2026-03-16 21:47:16 +0300

    Обновленны ошибки, и теперь можно ловить ошибки

--- routers.py
+++ routers.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status, HTTPException
 from models import NoteCreate
 from repositories import NoteRepository
 from services import NoteService
@@ -9,11 +9,22 @@ def get_service():
     repo = NoteRepository()
     return NoteService(repo)
 
-@router.post("/")
+@router.post("/", status_code=status.HTTP_201_CREATED)
 def create_note(payload: NoteCreate, service: NoteService = Depends(get_service)):
-    return service.create_note(payload.title, payload.body)
+    try:
+        return service.create_note(payload.title, payload.body)
+    except ValueError:
+        raise HTTPException(
+            status_code=422,
+            detail={"error": "Validation", "reason": "title_empty"}
+        )
 
 @router.get("/{note_id}")
 def get_note(note_id: int, service: NoteService = Depends(get_service)):
     note = service.get_note(note_id)
+    if not note:
+        raise HTTPException(
+            status_code=404,
+            detail={"error": "Not found", "reason": "note_not_found"}
+        )
     return note
