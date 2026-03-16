d87b64a57302a049147f9e5400a00ddf2bc33ee1
DarkUser1onion <matvejselivanov4@yandex.ru>
2026-03-16 21:50:41 +0300

    микрофикс

--- service.py
+++ service.py
@@ -7,9 +7,9 @@ class NoteService:
         self.repo = repo
 
     def create_note(self, title: str, body: Optional[str] = None) -> NoteOut:
-        if not title:
+        if not title or not title.strip():
             raise ValueError("title empty")
-        return self.repo.create(title=title, body=body)
+        return self.repo.create(title=title.strip(), body=body)
 
     def get_note(self, note_id: int) -> Optional[NoteOut]:
         return self.repo.get(note_id)
