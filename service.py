241548db3685a4d6efd78fc5c0df7b7f1510cfa5
DarkUser1onion <matvejselivanov4@yandex.ru>
2026-03-16 21:41:21 +0300

    Добавлен service.py и метод create_note

---
+++ service.py
@@ -0,0 +1,12 @@
+from typing import Optional
+from models import NoteOut
+from repositories import NoteRepository
+
+class NoteService:
+    def __init__(self, repo: NoteRepository):
+        self.repo = repo
+
+    def create_note(self, title: str, body: Optional[str] = None) -> NoteOut:
+        if not title:
+            raise ValueError("title empty")
+        return self.repo.create(title=title, body=body)
