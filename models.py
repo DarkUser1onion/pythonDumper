864686ad2238485c03448b4a14fb9638c329ac45
DarkUser1onion <matvejselivanov4@yandex.ru>
2026-03-16 21:57:57 +0300

    Бог в помощь

--- models.py
+++ models.py
@@ -1,12 +1,18 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 
-class NoteCreate(BaseModel):
-    title: str
-    body: str = ""
+class ProductCreate(BaseModel):
+    name: str = Field(..., min_length=2, max_length=80)
+    price: int = Field(..., ge=0)
+    in_stock: bool = True
 
-class NoteOut(BaseModel):
+class ProductUpdate(BaseModel):
+    name: Optional[str] = Field(None, min_length=2, max_length=80)
+    price: Optional[int] = Field(None, ge=0)
+    in_stock: Optional[bool] = None
+
+class ProductOut(BaseModel):
     id: int
-    title: str
-    body: Optional[str] = None
-    created_at: str
+    name: str
+    price: int
+    in_stock: bool
